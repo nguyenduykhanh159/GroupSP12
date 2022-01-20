2d60da7641a5498a18015f361513dab0d8c81a27
nguyenduykhanh159 <duykhanhctb@gmail.com>
2022-01-20 23:53:50 +0700

    add bean

--- src/main/java/com/example/demo/entity/CartProduct.java
+++ src/main/java/com/example/demo/entity/CartProduct.java
@@ -2,7 +2,6 @@ package com.example.demo.entity;
 
 import java.io.Serializable;
 
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
